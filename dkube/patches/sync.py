af0ff242fcd436e955bdf9bb6a19ca245b204ed0
Rajendra <rajendra.singh@oneconvergence.com>
2021-06-25 16:55:20 -0500

    disable visualization server

--- dkube/patches/sync.py
+++ dkube/patches/sync.py
@@ -1,11 +1,159 @@
+# Copyright 2020-2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import os
+import base64
+
+kfp_version = os.environ["KFP_VERSION"]
+disable_istio_sidecar = True
+mlpipeline_minio_access_key = base64.b64encode(
+    bytes(os.environ.get("MINIO_ACCESS_KEY"), 'utf-8')).decode('utf-8')
+mlpipeline_minio_secret_key = base64.b64encode(
+    bytes(os.environ.get("MINIO_SECRET_KEY"), 'utf-8')).decode('utf-8')
 
 
 class Controller(BaseHTTPRequestHandler):
     def sync(self, parent, children):
-        return {"status": {}, "children": []}
+        pipeline_enabled = parent.get("metadata", {}).get(
+            "labels", {}).get("pipelines.kubeflow.org/enabled")
 
+        if pipeline_enabled != "true":
+            return {"status": {}, "children": []}
+
+        # Compute status based on observed state.
+        desired_status = {
+            "kubeflow-pipelines-ready": \
+                len(children["ConfigMap.v1"]) == 1 and \
+                len(children["Deployment.apps/v1"]) == 1 and \
+                len(children["Service.v1"]) == 1 and \
+                "True" or "False"
+        }
+
+        # Generate the desired child object(s).
+        # parent is a namespace
+        namespace = parent.get("metadata", {}).get("name")
+        desired_resources = [
+            {
+                "apiVersion": "v1",
+                "kind": "ConfigMap",
+                "metadata": {
+                    "name": "metadata-grpc-configmap",
+                    "namespace": namespace,
+                },
+                "data": {
+                    "METADATA_GRPC_SERVICE_HOST":
+                    "metadata-grpc-service.kubeflow",
+                    "METADATA_GRPC_SERVICE_PORT": "8080",
+                },
+            },
+            # Artifact fetcher related resources below.
+            {
+                "apiVersion": "apps/v1",
+                "kind": "Deployment",
+                "metadata": {
+                    "labels": {
+                        "app": "ml-pipeline-ui-artifact"
+                    },
+                    "name": "ml-pipeline-ui-artifact",
+                    "namespace": namespace,
+                },
+                "spec": {
+                    "selector": {
+                        "matchLabels": {
+                            "app": "ml-pipeline-ui-artifact"
+                        }
+                    },
+                    "template": {
+                        "metadata": {
+                            "labels": {
+                                "app": "ml-pipeline-ui-artifact"
+                            },
+                            "annotations": disable_istio_sidecar and {
+                                "sidecar.istio.io/inject": "false"
+                            } or {},
+                        },
+                        "spec": {
+                            "containers": [{
+                                "name":
+                                "ml-pipeline-ui-artifact",
+                                "image":
+                                "gcr.io/ml-pipeline/frontend:" + kfp_version,
+                                "imagePullPolicy":
+                                "IfNotPresent",
+                                "ports": [{
+                                    "containerPort": 3000
+                                }],
+                                "resources": {
+                                    "requests": {
+                                        "cpu": "10m",
+                                        "memory": "70Mi"
+                                    },
+                                    "limits": {
+                                        "cpu": "100m",
+                                        "memory": "500Mi"
+                                    },
+                                }
+                            }],
+                            "serviceAccountName":
+                            "default-editor"
+                        }
+                    }
+                }
+            },
+            {
+                "apiVersion": "v1",
+                "kind": "Service",
+                "metadata": {
+                    "name": "ml-pipeline-ui-artifact",
+                    "namespace": namespace,
+                    "labels": {
+                        "app": "ml-pipeline-ui-artifact"
+                    }
+                },
+                "spec": {
+                    "ports": [{
+                        "name":
+                        "http",  # name is required to let istio understand request protocol
+                        "port": 80,
+                        "protocol": "TCP",
+                        "targetPort": 3000
+                    }],
+                    "selector": {
+                        "app": "ml-pipeline-ui-artifact"
+                    }
+                }
+            },
+        ]
+        print('Received request:', parent)
+        print('Desired resources except secrets:', desired_resources)
+        # Moved after the print argument because this is sensitive data.
+        desired_resources.append({
+            "apiVersion": "v1",
+            "kind": "Secret",
+            "metadata": {
+                "name": "mlpipeline-minio-artifact",
+                "namespace": namespace,
+            },
+            "data": {
+                "accesskey": mlpipeline_minio_access_key,
+                "secretkey": mlpipeline_minio_secret_key,
+            },
+        })
+
+        return {"status": desired_status, "children": desired_resources}
 
     def do_POST(self):
         # Serve the sync() function as a JSON webhook.
